a31bd836807b64770d352baf0f9398436682473b
wyt <80101277@qq.com>
2024-08-21 18:53:57 +0800

    add electron for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+import { join } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    electron({
+      // 主进程入口文件
+      entry: ["src-electron/main.ts", "src-electron/preload.ts"],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': join(__dirname, "src"),
+    }
+  }
 })
